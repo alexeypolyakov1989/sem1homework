e3f77b230f278be7d4652020ed6dc3af376cdc8d
Alexey <alexeypolyakov1989@yandex.ru>
2023-01-18 15:43:18 +0300

    add_hw_task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Программа определения четности числа");
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+} 
+
+else
+{
+    Console.WriteLine("Число нечетное");
+}
