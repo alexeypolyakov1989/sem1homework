d913b6a7ea2d9e67e7ffe62b0ad0c9002f6e0c72
Alexey <alexeypolyakov1989@yandex.ru>
2023-01-18 12:12:10 +0300

    add_task1

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Программа определения большего из двух чисел");
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine ("Первое число больше второго");
+}
+if (number1 < number2)
+{
+    Console.WriteLine ("Второе число больше первого");
+}
+if (number1 == number2)
+{
+    Console.WriteLine ("Два числа равны");
+}
